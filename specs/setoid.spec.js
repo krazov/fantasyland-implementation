6263601917ec5ba086bb0b0c8d740dfc5c9b0b65
Jon Krazov <krazov@interia.pl>
2020-02-08 13:03:26 +0100

    Expanding setoid first condition spec

--- specs/setoid.spec.js
+++ specs/setoid.spec.js
@@ -1,10 +1,26 @@
 import { setoid } from '../fantasy-land/setoid.js';
 
 describe('setoid', () => {
-    it('a[\'fantasy-land/equals\'](a) === true (reflexivity)', () => {
-        const setoid42 = setoid(42);
-        const actual = setoid42['fantasy-land/equals'](setoid42);
+    describe("a['fantasy-land/equals'](a) === true (reflexivity)", () => {
+        it('should work for number', () => {
+            const setoid63 = setoid(63);
+            const actual = setoid63['fantasy-land/equals'](setoid63);
 
-        expect(actual).toBe(true);
+            expect(actual).toBe(true);
+        });
+
+        it('should work for string', () => {
+            const setoid63 = setoid('63');
+            const actual = setoid63['fantasy-land/equals'](setoid63);
+
+            expect(actual).toBe(true);
+        });
+
+        it('should work for object', () => {
+            const setoid63 = setoid({ theNumber: 63 });
+            const actual = setoid63['fantasy-land/equals'](setoid63);
+
+            expect(actual).toBe(true);
+        });
     });
 });
