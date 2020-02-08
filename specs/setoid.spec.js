f88e9d7f86a62796ca38e9fc664e7fadfdce2344
Jon Krazov <krazov@interia.pl>
2020-02-08 15:16:53 +0100

    Setoid, condition 2: symmetry

--- specs/setoid.spec.js
+++ specs/setoid.spec.js
@@ -23,4 +23,43 @@ describe('setoid', () => {
             expect(actual).toBe(true);
         });
     });
+
+    describe("a['fantasy-land/equals'](b) === b['fantasy-land/equals'](a) (symmetry)", () => {
+        it('should work for the same setoid', () => {
+            const setoid63a = setoid(63);
+            const setoid63b = setoid63a;
+
+            const actual = setoid63a['fantasy-land/equals'](setoid63b) === setoid63b['fantasy-land/equals'](setoid63a);
+
+            expect(actual).toBe(true);
+        });
+
+        it('should work for different setoids carrying the same value (number)', () => {
+            const setoid63a = setoid(63);
+            const setoid63b = setoid(63);
+
+            const actual = setoid63a['fantasy-land/equals'](setoid63b) === setoid63b['fantasy-land/equals'](setoid63a);
+
+            expect(actual).toBe(true);
+        });
+
+        it('should work for different setoids carrying the same value (object)', () => {
+            const commonObject = { theNumber: 63 };
+            const setoid63a = setoid(commonObject);
+            const setoid63b = setoid(commonObject);
+
+            const actual = setoid63a['fantasy-land/equals'](setoid63b) === setoid63b['fantasy-land/equals'](setoid63a);
+
+            expect(actual).toBe(true);
+        });
+
+        it('should work for setoids carrying different values (object)', () => {
+            const setoid63a = setoid({ theNumber: 63 });
+            const setoid63b = setoid({ theNumber: 63 });
+
+            const actual = setoid63a['fantasy-land/equals'](setoid63b) === setoid63b['fantasy-land/equals'](setoid63a);
+
+            expect(actual).toBe(true);
+        });
+    });
 });
